6457fbddd0bb988ffcc471484d66254c4a2d090f
Ruzana khan <90472334+Ruzana-khan@users.noreply.github.com>
2023-12-22 12:25:03 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+K=0 
+C=0 
+F=0
+#celsius to kelvin
+k_c=C+273.15
+print("celsius to kelvin",k_c,"kelvin")
+#kelvin to celsius
+c_k=K-273.15
+print("kelvin to celsius",c_k,"kelvin")
+#celsius to farenheit
+c_f=((9/5)*C)+32
+print("celsius to farenheit",c_f,"farenheit")
+#farenheit to celsius
+f_c=((5/9)*(F-32))
+print("farenheit to celsius",f_c,"celsius")
+#kelvin to farenheit
+k_f=((9/5)*(K-273.15))+32
+print("kelvin to farenheit",k_f,"farenheit")
+#farenheit to kelvin
+f_k=((5/9)*(F-32))+273.15
+print("farenheit to kelvin",f_k,"kelvin")
